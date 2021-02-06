d6d8b3b69be6d647295094a4c9d0cd7a16c6e3c6
Athena Brisse <brisseathena@gmail.com>
2021-02-06 21:38:39 +0100

    Initial Commit

---
+++ Projet 3/Projet_3App.swift
@@ -0,0 +1,17 @@
+//
+//  Projet_3App.swift
+//  Projet 3
+//
+//  Created by Athena Brisse on 06/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct Projet_3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
